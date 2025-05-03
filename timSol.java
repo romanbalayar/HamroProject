c4e04e2269e8372df9ad25eae866cdb5d1c8419a
romanbalayar <127218955+romanbalayar@users.noreply.github.com>
2025-05-03 17:05:59 -0600

    Update timSol.java

--- timSol.java
+++ timSol.java
@@ -69,7 +69,7 @@ public class timSol {
         }
     }
 
-    // Print function like your style
+    // Print function 
     public static void printArr(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
